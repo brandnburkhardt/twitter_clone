ae30b0c57c31db3cde25ff440c371422a47ad07c
Brandon <brandon@membr.com>
2013-06-30 23:27:09 -0400

    spec refactoring

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,36 +2,58 @@ require 'spec_helper'
 
 describe "Static Pages" do
 
-  let(:base_title) { "Twitter Clone Sample App" }
-
   subject { page }
+
+  shared_examples_for "all static pages" do
+    it { should have_selector 'h1', :text => heading }
+    it { should have_selector 'title', :text => full_title(page_title) }
+  end
 
   describe "Home Page" do
     before { visit root_path }
+    let(:heading) { 'Twitter Clone' }
+    let(:page_title) { '' }
 
-    it { should have_selector 'h1', :text => 'Twitter Clone' }
-    it { should have_selector 'title', :text => full_title('') }
+    it_should_behave_like "all static pages"
     it { should_not have_selector 'title', :text => " | Home" }
   end
 
   describe "Help Page" do
     before { visit help_path }
+    let(:heading) { 'Help' }
+    let(:page_title) { 'Help' }
 
-    it { should have_selector 'h1', :text => 'Help' }
-    it { should have_selector 'title', :text => full_title('Help') }
+    it_should_behave_like "all static pages"
   end
 
   describe "About Page" do
     before { visit about_path }
+    let(:heading) { 'About' }
+    let(:page_title) { 'About' }
 
-    it { should have_selector 'h1', :text => 'About' }
-    it { should have_selector 'title', :text => full_title('About') }
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact Page" do
     before { visit contact_path }
+    let(:heading) { 'Contact Me' }
+    let(:page_title) { 'Contact Me' }
 
-    it { should have_selector 'h1', :text => 'Contact Me' }
-    it { should have_selector 'title', :text => full_title('Contact Me') }
+    it_should_behave_like "all static pages"
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', :text => full_title('About')
+    click_link "Help"
+    page.should have_selector 'title', :text => full_title('Help')
+    click_link "Contact"
+    page.should have_selector 'title', :text => full_title('Contact Me')
+    click_link "Home"
+    click_link "Sign Up Now"
+    page.should have_selector 'title', :text => full_title('')
+    click_link "Sample App"
+    page.should have_selector 'title', :text => full_title('')
   end
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,5 +1,2 @@
 
-def full_title(page_title)
-  base_title = "Twitter Clone Sample App"
-  page_title.empty? ? base_title : "#{base_title} | #{page_title}"
- end
+include ApplicationHelper
