18cb1a0de781163f9f40c11e8d6638a77603f559
Brandon <brandon@membr.com>
2013-07-04 15:58:44 -0400

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "User Created Successfully. Welcome to Twitter Clone!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def index
+  end
+  
+  def show
+    @user = User.find(params[:id])
   end
 end
